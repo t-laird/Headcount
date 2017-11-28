1ab926b3dde3dc8ad264b5d34630660ec7ce4e79
julie hawkins <julabi@gmail.com>
2017-11-27 20:13:03 -0700

    merged local master to branch card-basic-style

--- src/Search.js
+++ src/Search.js
@@ -1,24 +1,42 @@
 import React, { Component } from 'react';
+import './Styles/Search.css';
 
 class Search extends Component {
   constructor() {
     super();
 
     this.state = {
-      value: ''
+      value: '',
+      expanded: 'Search'
     }
-  } 
+  }
+
+  expandSearch = () => {
+    if (this.state.expanded === 'Search expanded') {
+      this.setState({
+        expanded: 'Search'
+      });
+    } else {
+      this.setState({
+        expanded: 'Search expanded'
+      });
+      this.searchInput.focus();
+
+    }
+  }
 
   render() {
     return (
-      <div className='Search'>
+      <div className={this.state.expanded}>
         <input 
           type='text' 
-          // value={this.state.value} 
+          // value={this.state.value}
+          ref={(sInput) => { this.searchInput = sInput; }}    
           onChange={(e) => {
             this.props.updateQuery(e.target.value);
           }}
           placeholder='Search'/>
+          <i className="icon-search" onClick={this.expandSearch}></i>     
       </div>
     )
   }
