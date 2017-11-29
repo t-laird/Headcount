340439087a805ae16b1014ee8bc76b8c6d12270f
Thomas Laird <t6r6l5@gmail.com>
2017-11-29 13:59:37 -0700

    basic card style change

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -8,37 +8,36 @@ const Card = (props) => {
       <li key={`li-${index}`} className={liStyle}>
         <p>{dataPoint}:</p> {props.data[dataPoint]}
       </li>
-    )
-  })
+    );
+  });
 
   return (
     <div className={props.type}>
-    {
-      props.district &&
-      <div onClick={() => {props.selectCard(props.district)}}>
-
-        <h4>{props.district}</h4>
-        <ul>
-          {mappedData}
-        </ul>
-      </div>
-    }
-    {
-      (props.type === "Card compared") &&
-      <div>
-        <h1>Click Two Cards to Compare Them</h1>
-      </div>
-    }
-    {
-      (props.type === "Card displayComparedData") &&
-      <div>
-        <h3> {props.district1}: {props.compareData[props.district1]}</h3>
-        <h3> {props.compareData.compared}</h3>
-        <h3> {props.district2}: {props.compareData[props.district2]}</h3>
-      </div>
-    }
+      {
+        props.district &&
+        <div onClick={() => {props.selectCard(props.district);}}>
+          <h4>{props.district}</h4>
+          <ul>
+            {mappedData}
+          </ul>
+        </div>
+      }
+      {
+        (props.type === "Card compared") &&
+          <div>
+            <h1>Click Two Cards to Compare Them</h1>
+          </div>
+      }
+      {
+        (props.type === "Card displayComparedData") &&
+        <div>
+          <h3> {props.district1}: {props.compareData[props.district1]}</h3>
+          <h3> {props.compareData.compared}</h3>
+          <h3> {props.district2}: {props.compareData[props.district2]}</h3>
+        </div>
+      }
     </div>
   );
-}
+};
 
 export default Card;
