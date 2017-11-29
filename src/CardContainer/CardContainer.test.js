b44706c6765f185104b820076bd073404eedc528
Thomas Laird <t6r6l5@gmail.com>
2017-11-28 19:03:23 -0700

    test for rendering correct # of cards

--- src/CardContainer/CardContainer.test.js
+++ src/CardContainer/CardContainer.test.js
@@ -4,13 +4,26 @@ import CardContainer from './CardContainer';
 import { shallow, mount } from 'enzyme';
 
 describe('CardContainer test', () => {
+  let renderedCardContainer;
+  beforeEach( () => {
+    renderedCardContainer = shallow(<CardContainer cards={[]}/>);
+  });
   it('should render the correct components', () => {
-    const renderedCardContainer = shallow(<CardContainer cards={[]}/>);
     expect(renderedCardContainer.find('.CardContainer').length).toEqual(1);
-  })
+  });
 
   it('should match the CardContainer snapshot', () => {
-    const renderedCardContainer = shallow(<CardContainer cards={[]}/>);
     expect(renderedCardContainer).toMatchSnapshot();
-  })
+  });
+
+  it('should render three cards when passed an array with 3 items in it ', () => {
+    const mockCards = [
+      { Location1: {2000: 0.5, 2001: 1} },
+      { Location2: {2000: 0.5, 2001: 1} },
+      { Location3: {2000: 0.5, 2001: 1} }
+    ];
+    renderedCardContainer = shallow(<CardContainer cards={mockCards} />);
+
+    expect(renderedCardContainer.find('Card').length).toEqual(3);
+  });
 });
