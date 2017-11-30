056bbe9f3408c3d8a55546214cdb1b55032ece15
julie hawkins <julabi@gmail.com>
2017-11-29 21:46:53 -0700

    cleans up test files

--- src/ComparisonContainer/ComparisonContainer.test.js
+++ src/ComparisonContainer/ComparisonContainer.test.js
@@ -5,24 +5,40 @@ import { shallow, mount } from 'enzyme';
 
 describe('ComparisonContainer test', () => {
   it('should render the correct components', () => {
-    const renderedComparisonContainer = shallow(<ComparisonContainer cards={[]} comparison={[null, null]} />);
+    const renderedComparisonContainer = shallow(
+      <ComparisonContainer 
+        cards={[]} 
+        comparison={[null, null]} />);
+
     expect(renderedComparisonContainer.find('.ComparisonContainer').length).toEqual(1);
   });
 
   it('should match the ComparisonContainer snapshot', () => {
-    const renderedComparisonContainer = shallow(<ComparisonContainer cards={[]} comparison={[null, null]}/>);
+    const renderedComparisonContainer = shallow(
+      <ComparisonContainer 
+        cards={[]} 
+        comparison={[null, null]}/>);
+
     expect(renderedComparisonContainer).toMatchSnapshot();
   });
 
   it('It should render the correct default cards when not given comparison data', () => {
-    const renderedComparisonContainer = mount(<ComparisonContainer cards={[]} comparison={[null, null]} />);
+    const renderedComparisonContainer = mount(
+      <ComparisonContainer 
+        cards={[]} 
+        comparison={[null, null]} />);
+
     expect(renderedComparisonContainer.find('Card').length).toEqual(3);
     expect(renderedComparisonContainer.find('.comparison').length).toEqual(2);
     expect(renderedComparisonContainer.find('.compared').length).toEqual(1);
   });
 
   it('Should render a standard card with a selected class but no middle comparison when passed a single valid comparison', () => {
-    const renderedComparisonContainer = mount(<ComparisonContainer cards={[{Colorado: {2003: 0.5, 2004: 0.6}}]} comparison={['Colorado', null]} />);
+    const renderedComparisonContainer = mount(
+      <ComparisonContainer 
+        cards={[{Colorado: {2003: 0.5, 2004: 0.6}}]} 
+        comparison={['Colorado', null]} />);
+
     expect(renderedComparisonContainer.find('.selected').length).toEqual(1);
     expect(renderedComparisonContainer.find('.comparison').length).toEqual(1);
     expect(renderedComparisonContainer.find('.compared').length).toEqual(1);
@@ -30,6 +46,7 @@ describe('ComparisonContainer test', () => {
 
   it('Should render two standard cards with the selected class and a middle comparison card when passed two valid comparisons', () => {
     const mockFunc = jest.fn();
+
     mockFunc.mockReturnValue(true);
     const renderedComparisonContainer = 
       mount(<ComparisonContainer 
@@ -42,6 +59,5 @@ describe('ComparisonContainer test', () => {
     expect(renderedComparisonContainer.find('.selected').length).toEqual(2);
     expect(renderedComparisonContainer.find('.comparison').length).toEqual(0);
     expect(renderedComparisonContainer.find('.displayComparedData').length).toEqual(1);
-    
   });
 });
--- src/Header/Header.test.js
+++ src/Header/Header.test.js
@@ -6,12 +6,14 @@ import { shallow, mount } from 'enzyme';
 describe('Header test', () => {
   it('should render the correct components', () => {
     const renderedHeader = shallow(<Header />);
+
     expect(renderedHeader.find('.Header').length).toEqual(1);
     expect(renderedHeader.find('h1').length).toEqual(1);
-  })
+  });
 
   it('should match the Header snapshot', () => {
     const renderedHeader = shallow(<Header />);
+
     expect(renderedHeader).toMatchSnapshot();
-  })
+  });
 });
