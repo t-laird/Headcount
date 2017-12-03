fc27ff3eec9bb0c46c8a3192fe7eb5b495ebc696
Thomas Laird <t6r6l5@gmail.com>
2017-12-03 13:32:45 -0700

    chart refactor for object.keys references and finding data max

--- src/Chart/Chart.js
+++ src/Chart/Chart.js
@@ -5,41 +5,47 @@ import Button from '../Button/Button';
 
 const Chart = (props) => {
   const comparedLocations = props.cards.filter( card => {
-    return Object.keys(card)[0] === props.comparisons[0] || Object.keys(card)[0] === props.comparisons[1];
+    const district = Object.keys(card)[0];
+
+    return district === props.comparisons[0] || district === props.comparisons[1];
   });
+  
+  const district1Name = Object.keys(comparedLocations[0])[0];
+  const district2Name = Object.keys(comparedLocations[1])[0];
+  const district1 = comparedLocations[0][district1Name];
+  const district2 = comparedLocations[1][district2Name];
+
   const location1 = Object.assign({}, 
-    {district: Object.keys(comparedLocations[0])[0]}, 
-    {scores: Object.values(comparedLocations[0][Object.keys(comparedLocations[0])[0]])}, 
-    {dates: Object.keys(comparedLocations[0][Object.keys(comparedLocations[0])[0]])}
+    {district: district1Name}, 
+    {scores: Object.values(district1)}, 
+    {dates: Object.keys(district1)}
   );
   const location2 = 
     Object.assign({}, 
-      {district: Object.keys(comparedLocations[1])[0]}, 
-      {scores: Object.values(comparedLocations[1][Object.keys(comparedLocations[1])[0]])}, 
-      {dates: Object.keys(comparedLocations[1][Object.keys(comparedLocations[1])[0]])}
+      {district: district2Name}, 
+      {scores: Object.values(district2)}, 
+      {dates: Object.keys(district2)}
     );
+
   const mappedDates = location1.dates.map( (date, index) => {
     if (location1.dates.length > 10) {
-      return <li style={{width: 600/location1.dates.length + 'px', fontSize: '10px'}} key={index}>{date}</li>;
+      return (
+        <li 
+          style={{width: 600/location1.dates.length + 'px', fontSize: '10px'}} 
+          key={index}>{date}
+        </li>
+      );
     } else {
-      return <li style={{width: 600/location1.dates.length + 'px'}} key={index}>{date}</li>;
-    }
-  });
-  let dataMax = 0;
-
-  location1.scores.forEach( score => {
-    if (score > dataMax) {
-      dataMax = score;
-    }
-  });
-
-  location2.scores.forEach( score => {
-    if (score > dataMax) {
-      dataMax = score;
+      return (
+        <li 
+          style={{width: 600/location1.dates.length + 'px'}} 
+          key={index}>{date}
+        </li>
+      );
     }
   });
 
-  dataMax = Math.max(1, dataMax);
+  const dataMax = Math.max(1, ...location1.scores, ...location2.scores);
 
   return (
     <div className="Chart">
@@ -48,17 +54,34 @@ const Chart = (props) => {
           <span>0</span>
           <span>{dataMax}</span>
         </div>
-        <Sparklines height={50} width={100} max={dataMax} min={0} data={location1.scores}>
-          <SparklinesLine color="red" style={{fill: 'none'}}/>
+        <Sparklines 
+          min={0} 
+          height={50} 
+          width={100} 
+          max={dataMax} 
+          data={location1.scores}>
+          <SparklinesLine 
+            color="red" 
+            style={{fill: 'none'}} />
         </Sparklines>
-        <Sparklines height={50} width={100} max={dataMax} min={0} data={location2.scores}>
-          <SparklinesLine color="blue" style={{fill: 'none'}}/>
+        <Sparklines 
+          min={0} 
+          height={50} 
+          width={100} 
+          max={dataMax} 
+          data={location2.scores}>
+          <SparklinesLine 
+            color="blue" 
+            style={{fill: 'none'}} />
         </Sparklines>
       </div>
       <div className="chartNav">
         <h5>{location1.district}<div className="district1key"></div></h5>
         <h5>{location2.district}<div className="district2key"></div></h5>
-        <Button type="chart-close" name="Close" chartStatus={props.chartStatus}/>
+        <Button 
+          name="Close" 
+          type="chart-close" 
+          chartStatus={props.chartStatus} />
       </div>
       <ul>
         {mappedDates}
